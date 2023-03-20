c7edb5218cc84774a874c0648d41f4c95b291fb9
Sebastian <44552816+bazeroni@users.noreply.github.com>
2023-03-19 21:19:42 -0400

    update prefixes

--- iva.py
+++ iva.py
@@ -495,7 +495,7 @@ async def on_message(message):
                     guild_memory = chat_mems[channel_id]
                     guild_memory.max_token_limit = 512
                     guild_memory.ai_prefix = f"Iva ({agent_mention})"
-                    #guild_memory.human_prefix = f"{user_name} ({user_mention})"
+                    guild_memory.human_prefix = f"{user_name} ({user_mention})"
                     
                 else:
 
@@ -505,7 +505,7 @@ async def on_message(message):
                         memory_key="chat_history",
                         input_key="input",
                         ai_prefix = f"Iva ({agent_mention})",
-                        #human_prefix = f"{user_name} ({user_mention})",
+                        human_prefix = f"{user_name} ({user_mention})",
                     )
                 
                 llm_chain = LLMChain(
