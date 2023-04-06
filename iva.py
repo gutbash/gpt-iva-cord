0938f8cc8958339c4b4e93496bb9aa5394e4a098
bash <4gutzees@gmail.com>
2023-04-06 17:53:57 -0400

    set default to gpt-4

--- iva.py
+++ iva.py
@@ -460,7 +460,7 @@ async def iva(interaction: discord.Interaction, prompt: str, file: discord.Attac
         prompt = prompt.replace("—v3", "")
         chat_model = "gpt-3.5-turbo"
     else:
-        chat_model = "gpt-3.5-turbo"
+        chat_model = "gpt-4"
     
     # Get the current timestamp
     timestamp = datetime.datetime.now()
