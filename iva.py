a9338cab7b89fdd7fb1faf281c953f94dc02b8a5
bash <4gutzees@gmail.com>
2023-04-09 23:04:42 -0400

    add rich embed links at end of ask response

--- iva.py
+++ iva.py
@@ -992,6 +992,11 @@ async def iva(interaction: discord.Interaction, prompt: str, file: discord.Attac
             #print(files, embeds)
             if len(embeds_overflow) > 0:
                 await interaction.channel.send(files = files_overflow, embeds=embeds_overflow)
+            pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+            links = re.findall(pattern, reply)
+            if len(links) > 0:
+                formatted_links = "\n".join(links)
+                await interaction.channel.send(content=formatted_links)
             return
         except Exception as e:
             print(e)
