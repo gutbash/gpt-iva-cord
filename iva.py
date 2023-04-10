5e3d560129b899e718cdba5b421ebe90dafea82b
bash <4gutzees@gmail.com>
2023-04-10 00:54:57 -0400

    set max iters to 1

--- iva.py
+++ iva.py
@@ -743,7 +743,7 @@ async def iva(interaction: discord.Interaction, prompt: str, file: discord.Attac
             memory=memory,
             ai_prefix=f"Iva",
             llm_prefix=f"Iva",
-            max_iterations=3,
+            max_iterations=1,
             early_stopping_method="generate",
             return_intermediate_steps=False
         )
