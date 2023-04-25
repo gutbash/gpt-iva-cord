52912652295f8b6b915f0eac27e251db61d6584b
Sebastian <44552816+bazeroni@users.noreply.github.com>
2023-04-25 15:34:07 -0400

    update directories for web app update requirements

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn web:app
+web: gunicorn app:app
 worker: python iva.py
--- web.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/tos')
-def tos():
-    return render_template('tos/tos.html')
