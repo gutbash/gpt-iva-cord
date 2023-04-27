6eb5d6b28e2b51f3b583a416343cd330c6637c8d
bash <4gutzees@gmail.com>
2023-04-27 17:10:08 -0400

    update padding methods for encrypt

--- utils/crypto_utils.py
+++ utils/crypto_utils.py
@@ -29,14 +29,28 @@ def decrypt_data(encrypted_data: bytes, key: bytes) -> bytes:
     unpadder = padding.PKCS7(128).unpadder()
     return unpadder.update(data) + unpadder.finalize()
 
+def encrypt_key(key: bytes, master_key: bytes) -> bytes:
+    backend = default_backend()
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(master_key), modes.CBC(iv), backend=backend)
+    encryptor = cipher.encryptor()
+    encrypted_key = encryptor.update(key) + encryptor.finalize()
+    return iv + encrypted_key
+
+def decrypt_key(encrypted_key: bytes, master_key: bytes) -> bytes:
+    backend = default_backend()
+    iv, encrypted_key = encrypted_key[:16], encrypted_key[16:]
+    cipher = Cipher(algorithms.AES(master_key), modes.CBC(iv), backend=backend)
+    decryptor = cipher.decryptor()
+    return decryptor.update(encrypted_key) + decryptor.finalize()
+
 def envelope_encrypt(data: bytes, master_key: bytes) -> bytes:
     data_encryption_key = generate_data_encryption_key()
     encrypted_data = encrypt_data(data, data_encryption_key)
-    encrypted_data_key = encrypt_data(data_encryption_key, master_key)  # <-- Fixed
+    encrypted_data_key = encrypt_key(data_encryption_key, master_key)  # <-- Fixed
     return encrypted_data_key + encrypted_data
 
-
 def envelope_decrypt(encrypted_data: bytes, master_key: bytes) -> bytes:
-    encrypted_data_key, encrypted_data = encrypted_data[:48], encrypted_data[48:]
-    data_encryption_key = decrypt_data(encrypted_data_key, master_key)  # <-- This is correct
+    encrypted_data_key, encrypted_data = encrypted_data[:48], encrypted_data[48:]  # <-- This is correct
+    data_encryption_key = decrypt_key(encrypted_data_key, master_key)  # <-- Fixed
     return decrypt_data(encrypted_data, data_encryption_key)
