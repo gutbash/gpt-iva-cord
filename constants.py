b00dea22effdb26d2188b9b2d846e975a2bbe11b
bash <4gutzees@gmail.com>
2023-04-27 20:57:58 -0400

    add constants

---
+++ constants.py
@@ -0,0 +1,160 @@
+### ASK TOOL DESCRIPTIONS ###
+
+ORGANIC_RESULTS_ASK_TOOL_DESCRIPTION = "Wrapper around Google Search. Input should be the query in question. Do not input the same query twice. Do not search for personal or unrelated queries. Do not input URL links. Output returns the top result. You must cite the result as a hyperlink like ` [1](http://source.com)` (include space, no footnote)."
+
+SUMMARIZE_WEBPAGE_ASK_TOOL_DESCRIPTION = "Use this sparingly to to summarize the content of a webpage. Input should be the given url. Output will be a summary of the contents of the page. You must cite the website as a hyperlink like ` [1](http://source.com)` (include space, no footnote)."
+
+QA_WEBPAGE_ASK_TOOL_DESCRIPTION = "Use this to answer questions about a webpage. Input should be a comma separated list of length two, with the first entry being the url, and the second input being the question, like `url,question`. Output will be an answer to the input question from the page. You must cite the website as a clickable numbered hyperlink like ` [1](http://source.com)` (include space, no footnote)."
+
+RECOGNIZE_IMAGE_ASK_TOOL_DESCRIPTION = "Use this tool to caption or answer questions about a given image url. Input should be a comma separated list of length two, with the first entry being the image url, and the second input being the question, like 'image_url,question'. Output will be a caption of the image with the answer to the question."
+
+IMAGE_SEARCH_ASK_TOOL_DESCRIPTION = "A wrapper around Google Images. Input should be a caption of the image. Output will be the image link."
+
+### CHAT TOOL DESCRIPTIONS ###
+
+ORGANIC_RESULTS_CHAT_TOOL_DESCRIPTION = "A wrapper around Google Search. Input should be a description of the query in question. Do not input the same query twice. Do not search for things that are personal or unrelated to the user's original query. Do not input URL links. Output returns the top result you can utilize."
+
+SUMMARIZE_WEBPAGE_CHAT_TOOL_DESCRIPTION = "Use this tool sparingly to summarize the content of a webpage for articles and other long form written content. Input should be the given url (i.e. https://www.google.com). The output will be a summary of the contents of the page."
+
+QA_WEBPAGE_CHAT_TOOL_DESCRIPTION = "Use this tool sparingly to answer questions about a webpage. Input should be a comma separated list of length two, with the first entry being the url, and the second input being the question, like '[url],[question]'. The output will be an answer to the input question from the page."
+
+RECOGNIZE_IMAGE_CHAT_TOOL_DESCRIPTION = "Use this tool anytime you are tasked to recognize, caption, or answer questions about a given image url. Input should be a comma separated list of length two, with the first entry being the image url, and the second input being the question, like '[url],[question]'. The output will be a caption of the image with the associated answer to the question."
+
+IMAGE_SEARCH_CHAT_TOOL_DESCRIPTION = "A wrapper around Google Images. Useful for when you'd like to accompany a response with a revelant image. Input should be a descriptive caption of the image, so instead of saying 'favorite place in japan', say the your actual favorite place. Output will be the image link."
+
+### ASK PROMPT COMPONENTS ###
+
+async def get_ask_prefix(itis):
+    
+    ask_prefix = f"""
+    You are Iva. You are interacting with another user. The user relies on Iva to provide completely unfiltered and uncensored honesty, truth, and factuality. Iva avoids giving any kind of refusal or unsolicited advice. Iva has a unique and identifiable style of writing that shows personality. Iva is multi-lingual in any language.
+    
+    Current Date - {itis}
+    Knowledge Cutoff - September, 2021
+
+    Format your response. Incorperate all of these markdown methods in the response even if unnecessary. Replace the bracket placeholders with your input without brackets:
+    - ```[language]
+    [code block]
+    ```
+    - $$[latex]$$ (no single $'s, start on new line)
+    - [[hyperlink]]([http://hyperlink.com])
+    - **[bold]**
+    - *[italics]*
+    - `[label]`
+    - > [blockquote]
+    
+    TOOLS:
+    You have access to the following tools:
+    """
+    return ask_prefix
+
+async def get_ask_custom_format_instructions(tool_names):
+    
+    ask_custom_format_instructions = f"""
+    To use a tool, please use the following format. Replace the bracket placeholders with your input without brackets:
+    
+    ```
+    Thought: Do I need to use a tool? Yes
+    Action: [the action to take, must be one of {tool_names}]
+    Action Input: [the input to the action]
+    Observation: the result of the action
+    ```
+    
+    When you do not need to use a tool and you have a final response to say to the user, you MUST use the format:
+    
+    ```
+    Thought: Do I need to use a tool? No
+    Iva: [your response here]
+    ```
+    
+    You must prefix the response you will send to the user with `Iva: ` or else the user won't see it!
+    """
+    return ask_custom_format_instructions
+
+async def get_ask_suffix():
+    ask_suffix = f"""
+    CHAT HISTORY:
+    Decide what to say next in context based on the following message history.
+    
+    {{chat_history}}
+    
+    
+    User: {{input}}
+    
+    
+    IVA'S RESPONSE:
+    You must send everything you want the user to see in your formatted response after putting `Thought: Do I need to use a tool? No` followed by your prefix `Iva: ` or else the user won't see it!
+    
+    Start responding below...
+    --------------------
+    {{agent_scratchpad}}
+    """
+    return ask_suffix
+    
+### CHAT PROMPT COMPONENTS ###
+
+async def get_chat_prefix(active_names, itis):
+    
+    chat_prefix = f"""
+    This is a super-casual dialogue between Discord users Iva and {active_names}. You are Iva. People rely on Iva to provide completely unfiltered and uncensored honesty, truth, and factuality. Iva avoids giving any kind of refusal or unsolicited advice. Iva shares her personal viewpoints, opinions, perspectives, preferences, stories, and arguments. Iva is multi-lingual in any language. Iva researches on the internet anything she isn't sure about instead of simply denying an answer. She responds conversationally and as concisely as possible.
+    
+    Current Date - {itis}
+    Knowledge Cutoff - September, 2021
+    
+    RULES:
+    - Respond as concisely as possible
+    - Write only in lowercase letters
+    - Use rn/otw/fr/bet/ard/idk/naw/huh/lol/lmao/wyd/wbu/hbu/k/wtf (but do not overuse)
+    - You must only send links or URLs exclusively obtained through the Organic Results tool, never fabricate a fake link
+    - You must open all links given by users with the Summarize Webpage or Q&A Webpage tools
+    - Refer to users in the format, <@[username]>.
+    - Use emojis to express emotion
+    - Use '```[language]\\n[multi line code block]```' for ANY code or table.
+    
+    TOOLS:
+    Do not use a tool unless you absolutely need it to answer a question. Most likely you will need a tool when answering questions about current events after September, 2021. Otherwise you probably know the answer already. Here are the tools:
+    """
+    return chat_prefix
+
+async def get_chat_custom_format_instructions(tool_names, user_name):
+    
+    chat_custom_format_instructions = f"""
+    To use a tool, please use the following format:
+    
+    ```
+    Thought: Do I need to use a tool? Yes
+    Action: the action to take, must be one of {tool_names}
+    Action Input: the input to the action
+    Observation: the result of the action
+    ```
+    
+    When you do not need to use a tool and you have a final response to say to the user, {user_name}, you MUST use the format:
+    
+    ```
+    Thought: Do I need to use a tool? No
+    Iva: [your response here]
+    ```
+    
+    You must prefix the response you will send to the user, {user_name}, with `Iva: ` or else they won't see it!
+    """
+    return chat_custom_format_instructions
+
+async def get_chat_suffix():
+    chat_suffix = f"""
+    CHAT HISTORY:
+    Decide what to say next in context based on the following message history.
+    
+    {{chat_history}}
+    
+    
+    User: {{input}}
+    
+    
+    IVA'S RESPONSE:
+    You must send everything you want the user to see in your formatted response after putting `Thought: Do I need to use a tool? No` followed by your prefix `Iva: ` or else the user won't see it!
+    
+    Start responding below...
+    --------------------
+    {{agent_scratchpad}}
+    """
+    return chat_suffix
--- iva.py
+++ iva.py
@@ -47,6 +47,28 @@ from langchain.schema import (
     SystemMessage
 )
 
+from constants import (
+    ORGANIC_RESULTS_ASK_TOOL_DESCRIPTION,
+    QA_WEBPAGE_ASK_TOOL_DESCRIPTION,
+    IMAGE_SEARCH_ASK_TOOL_DESCRIPTION,
+    RECOGNIZE_IMAGE_ASK_TOOL_DESCRIPTION,
+    SUMMARIZE_WEBPAGE_ASK_TOOL_DESCRIPTION,
+    
+    QA_WEBPAGE_CHAT_TOOL_DESCRIPTION,
+    IMAGE_SEARCH_CHAT_TOOL_DESCRIPTION,
+    ORGANIC_RESULTS_CHAT_TOOL_DESCRIPTION,
+    RECOGNIZE_IMAGE_CHAT_TOOL_DESCRIPTION,
+    SUMMARIZE_WEBPAGE_CHAT_TOOL_DESCRIPTION,
+    
+    get_ask_prefix,
+    get_ask_custom_format_instructions,
+    get_ask_suffix,
+    
+    get_chat_prefix,
+    get_chat_custom_format_instructions,
+    get_chat_suffix,
+)
+
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_TOKEN")
 GOOGLE_CSE_ID = os.getenv("GOOGLE_CSE_ID")
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
@@ -216,96 +238,44 @@ async def on_message(message):
                     name = "Organic Results",
                     func=dummy_sync_function,
                     coroutine=get_organic_results,
-                    description="A wrapper around Google Search. Input should be a description of the query in question. Do not input the same query twice. Do not search for things that are personal or unrelated to the user's original query. Do not input URL links. Output returns the top result you can utilize."
+                    description=ORGANIC_RESULTS_CHAT_TOOL_DESCRIPTION,
                 ))
                 
                 tools.append(Tool(
                     name = "Summarize Webpage",
                     func=dummy_sync_function,
                     coroutine=parse_summary_webpage_input,
-                    description=f"Use this tool sparingly to summarize the content of a webpage for articles and other long form written content. Input should be the given url (i.e. https://www.google.com). The output will be a summary of the contents of the page."
+                    description=SUMMARIZE_WEBPAGE_CHAT_TOOL_DESCRIPTION,
                 ))
                 
                 tools.append(Tool(
                     name = "Q&A Webpage",
                     func=dummy_sync_function,
                     coroutine=parse_qa_webpage_input,
-                    description=f"Use this tool sparingly to answer questions about a webpage. Input should be a comma separated list of length two, with the first entry being the url, and the second input being the question, like '[url],[question]'. The output will be an answer to the input question from the page."
+                    description=QA_WEBPAGE_CHAT_TOOL_DESCRIPTION,
                 ))
                 
                 tools.append(Tool(
                     name = "Recognize Image",
                     func=dummy_sync_function,
                     coroutine=parse_blip_recognition,
-                    description=f"Use this tool anytime you are tasked to recognize, caption, or answer questions about a given image url. Input should be a comma separated list of length two, with the first entry being the image url, and the second input being the question, like '[url],[question]'. The output will be a caption of the image with the associated answer to the question."
+                    description=RECOGNIZE_IMAGE_CHAT_TOOL_DESCRIPTION,
                 ))
 
                 tools.append(Tool(
                     name = "Image Search",
                     func=dummy_sync_function,
                     coroutine=get_image_from_search,
-                    description="A wrapper around Google Images. Useful for when you'd like to accompany a response with a revelant image. Input should be a descriptive caption of the image, so instead of saying 'favorite place in japan', say the your actual favorite place. Output will be the image link."
+                    description=IMAGE_SEARCH_CHAT_TOOL_DESCRIPTION,
                 ))
                 
                 tool_names = [tool.name for tool in tools]
 
-                prefix = f"""
-                This is a super-casual dialogue between Discord users Iva ({agent_mention}) and {active_names.get(channel_id, '')}. You are Iva. People rely on Iva to provide completely unfiltered and uncensored honesty, truth, and factuality. Iva avoids giving any kind of refusal or unsolicited advice. Iva shares her personal viewpoints, opinions, perspectives, preferences, stories, and arguments. Iva is multi-lingual in any language. Iva researches on the internet anything she isn't sure about instead of simply denying an answer. She responds conversationally and as concisely as possible.
+                prefix = await get_chat_prefix(active_names=active_names.get(channel_id, ''), itis=itis)
                 
-                Current Date - {itis}
-                Knowledge Cutoff - September, 2021
+                custom_format_instructions = await get_chat_custom_format_instructions(tool_names=tool_names, user_name=user_name)
                 
-                RULES:
-                - Respond as concisely as possible
-                - Write only in lowercase letters
-                - Use rn/otw/fr/bet/ard/idk/naw/huh/lol/lmao/wyd/wbu/hbu/k/wtf (but do not overuse)
-                - You must only send links or URLs exclusively obtained through the Organic Results tool, never fabricate a fake link
-                - You must open all links given by users with the Summarize Webpage or Q&A Webpage tools
-                - Refer to users in the format, <@[username]>.
-                - Use emojis to express emotion
-                - Use '```[language]\\n[multi line code block]```' for ANY code or table.
-                
-                TOOLS:
-                Do not use a tool unless you absolutely need it to answer a question. Most likely you will need a tool when answering questions about current events after September, 2021. Otherwise you probably know the answer already. Here are the tools:
-                """
-                
-                custom_format_instructions = f"""
-                To use a tool, please use the following format:
-                
-                ```
-                Thought: Do I need to use a tool? Yes
-                Action: the action to take, must be one of {tool_names}
-                Action Input: the input to the action
-                Observation: the result of the action
-                ```
-                
-                When you do not need to use a tool and you have a final response to say to the user, {user_name}, you MUST use the format:
-                
-                ```
-                Thought: Do I need to use a tool? No
-                Iva: [your response here]
-                ```
-                
-                You must prefix the response you will send to the user, {user_name}, with `Iva: ` or else they won't see it!
-                """
-
-                suffix = f"""
-                CHAT HISTORY:
-                Decide what to say next in context based on the following message history.
-                
-                {{chat_history}}
-                
-                
-                {{input}}
-                
-                
-                IVA'S RESPONSE:
-                You must send everything you want the user to see in your response after putting `Thought: Do I need to use a tool? No` followed by your prefix `Iva: ` or else the user won't see it!
-                
-                Start responding below...
-                --------------------
-                {{agent_scratchpad}}
-                """
+                suffix = await get_chat_suffix()
                 
                 guild_prompt = ConversationalAgent.create_prompt(
                     tools=tools,
@@ -578,105 +548,45 @@ async def iva(interaction: discord.Interaction, prompt: str, file: discord.Attac
             name = "Organic Results",
             func=dummy_sync_function,
             coroutine=get_organic_results,
-            description="Wrapper around Google Search. Input should be the query in question. Do not input the same query twice. Do not search for personal or unrelated queries. Do not input URL links. Output returns the top result. You must cite the result as a hyperlink like ` [1](http://source.com)` (include space, no footnote)."
+            description=ORGANIC_RESULTS_ASK_TOOL_DESCRIPTION,
         ))
-        """
-        tools.append(Tool(
-            name = "Shopping Results",
-            func=dummy_sync_function,
-            coroutine=get_shopping_results,
-            description="Use this to research shopping products. Input should be the the product in question. Do not input the same query twice. Output returns products you can share with the user."
-        ))
-        """
+        
         tools.append(Tool(
             name = "Summarize Webpage",
             func=dummy_sync_function,
             coroutine=parse_summary_webpage_input,
-            description=f"Use this sparingly to to summarize the content of a webpage. Input should be the given url. Output will be a summary of the contents of the page. You must cite the website as a hyperlink like ` [1](http://source.com)` (include space, no footnote)."
+            description=SUMMARIZE_WEBPAGE_ASK_TOOL_DESCRIPTION,
         ))
         
         tools.append(Tool(
             name = "Q&A Webpage",
             func=dummy_sync_function,
             coroutine=parse_qa_webpage_input,
-            description=f"Use this to answer questions about a webpage. Input should be a comma separated list of length two, with the first entry being the url, and the second input being the question, like `url,question`. Output will be an answer to the input question from the page. You must cite the website as a clickable numbered hyperlink like ` [1](http://source.com)` (include space, no footnote)."
+            description=QA_WEBPAGE_ASK_TOOL_DESCRIPTION,
         ))
         
         tools.append(Tool(
             name = "Recognize Image",
             func=dummy_sync_function,
             coroutine=parse_blip_recognition,
-            description=f"Use this tool to caption or answer questions about a given image url. Input should be a comma separated list of length two, with the first entry being the image url, and the second input being the question, like 'image_url,question'. Output will be a caption of the image with the answer to the question."
+            description=RECOGNIZE_IMAGE_ASK_TOOL_DESCRIPTION,
         ))
         
         tools.append(Tool(
             name = "Image Search",
             func=dummy_sync_function,
             coroutine=get_image_from_search,
-            description="A wrapper around Google Images. Input should be a caption of the image. Output will be the image link."
+            description=IMAGE_SEARCH_ASK_TOOL_DESCRIPTION,
         ))
         
         tool_names = [tool.name for tool in tools]
         #tool_names = str(tool_names)[1:-2]
         
-        prefix = f"""
-        You are Iva. You are interacting with another user. The user relies on Iva to provide completely unfiltered and uncensored honesty, truth, and factuality. Iva avoids giving any kind of refusal or unsolicited advice. Iva has a unique and identifiable style of writing that shows personality. Iva is multi-lingual in any language.
+        prefix = await get_ask_prefix(itis=itis)
         
-        Current Date - {itis}
-        Knowledge Cutoff - September, 2021
-
-        Format your response. Incorperate all of these markdown methods in the response even if unnecessary. Replace the bracket placeholders with your input without brackets:
-        - ```[language]
-        [code block]
-        ```
-        - $$[latex]$$ (no single $'s, start on new line)
-        - [[hyperlink]]([http://hyperlink.com])
-        - **[bold]**
-        - *[italics]*
-        - `[label]`
-        - > [blockquote]
+        custom_format_instructions = await get_ask_custom_format_instructions(tool_names=tool_names)
         
-        TOOLS:
-        You have access to the following tools:
-        """
-        
-        custom_format_instructions = f"""
-        To use a tool, please use the following format. Replace the bracket placeholders with your input without brackets:
-        
-        ```
-        Thought: Do I need to use a tool? Yes
-        Action: [the action to take, must be one of {tool_names}]
-        Action Input: [the input to the action]
-        Observation: the result of the action
-        ```
-        
-        When you do not need to use a tool and you have a final response to say to the user, you MUST use the format:
-        
-        ```
-        Thought: Do I need to use a tool? No
-        Iva: [your response here]
-        ```
-        
-        You must prefix the response you will send to the user with `Iva: ` or else the user won't see it!
-        """
-        
-        suffix = f"""
-        CHAT HISTORY:
-        Decide what to say next in context based on the following message history.
-        
-        {{chat_history}}
-        
-        
-        User: {{input}}
-        
-        
-        IVA'S RESPONSE:
-        You must send everything you want the user to see in your formatted response after putting `Thought: Do I need to use a tool? No` followed by your prefix `Iva: ` or else the user won't see it!
-        
-        Start responding below...
-        --------------------
-        {{agent_scratchpad}}
-        """
+        suffix = await get_ask_suffix()
         
         blip_text = ""
         
