c85223a3e87e10acd36c2832845c15dd0091e2a3
bash <4gutzees@gmail.com>
2023-04-29 16:11:33 -0400

    update thread send nest

--- iva.py
+++ iva.py
@@ -933,7 +933,7 @@ async def iva(interaction: discord.Interaction, prompt: str, file: discord.Attac
                     name=f"{user_name}'s thread with iva",
                 )
             
-                await interaction.followup.send(files=files, embeds=embeds, view=view, thread=thread)
+            await interaction.followup.send(files=files, embeds=embeds, view=view, thread=thread)
             
             last_response[channel_id][user_id] = interaction
             
