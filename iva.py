e79140db486ea79c343962e367c74fc20a300089
bash <4gutzees@gmail.com>
2023-04-29 16:52:13 -0400

    sleep wait for thread

--- iva.py
+++ iva.py
@@ -16,6 +16,7 @@ from tools import (
     get_full_blip,
 )
 
+import asyncio
 import os
 import openai
 import datetime
@@ -934,6 +935,7 @@ async def iva(interaction: discord.Interaction, prompt: str, file: discord.Attac
                     type=discord.ChannelType.public_thread,
                     name=f"{user_name}'s thread with iva",
                 )
+                await asyncio.sleep(3)
                 #await channel.send(files=files, embeds=embeds, view=view)
                 await interaction.followup.send(files=files, embeds=embeds, view=view, thread=channel)
             
