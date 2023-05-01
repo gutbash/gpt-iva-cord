42a4dddfb0ba01d720da328fafd2a0900772df02
Sebastian <44552816+bazeroni@users.noreply.github.com>
2023-05-01 03:01:29 -0400

    edit output parsing

--- iva.py
+++ iva.py
@@ -344,7 +344,8 @@ async def on_message(message):
                 try:
 
                     reply = await agent_chain.arun(input=f"{user_name} ({user_mention}): {prompt}{caption}")
-                    reply = reply.replace("Do I need to use a tool? No", "").replace("Iva: ", "")
+                    reply = reply.replace("Iva: ", "")
+                    reply = reply.replace("Do I need to use a tool? No", "")
 
                 except Exception as e:
                     if str(e).startswith("Could not parse LLM output:"):
