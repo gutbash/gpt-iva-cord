41b6da200d892a29c22c8e5c82df1e22c998de53
bash <4gutzees@gmail.com>
2023-05-08 11:14:11 -0400

    increase autopep8 aggression level remove non async func on repl

--- iva.py
+++ iva.py
@@ -695,25 +695,11 @@ async def iva(interaction: discord.Interaction, prompt: str, file_one: discord.A
             globals: Optional[Dict] = Field(default_factory=dict, alias="_globals")
             locals: Optional[Dict] = Field(default_factory=dict, alias="_locals")
 
-            def run(self, command: str) -> str:
-                
-                command = autopep8.fix_code(command.strip().strip("```").replace(":\n", ":\n    "), options={"aggressive": 1})
-                
-                """Run command with own globals/locals and returns anything printed."""
-                old_stdout = sys.stdout
-                sys.stdout = mystdout = StringIO()
-                try:
-                    exec(command, self.globals, self.locals)
-                    sys.stdout = old_stdout
-                    output = mystdout.getvalue()
-                except Exception as e:
-                    sys.stdout = old_stdout
-                    output = str(e)
-                return output
-
             async def arun(self, command: str) -> str:
                 
-                command = autopep8.fix_code(command.strip().strip("```").replace(":\n", ":\n    "), options={"aggressive": 1})
+                command = autopep8.fix_code(command.strip().strip("```"), options={"aggressive": 2})
+                
+                logging.info(command)
                 
                 """Run command (sync or async) with own globals/locals and returns anything printed."""
                 old_stdout = sys.stdout
