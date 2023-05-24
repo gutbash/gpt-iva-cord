a7422530febd34b823821c3d30508e3762a12687
bash <4gutzees@gmail.com>
2023-05-23 21:44:26 -0400

    update HTML list

--- utils/tool_utils.py
+++ utils/tool_utils.py
@@ -70,7 +70,7 @@ async def get_important_text(url):
                 #print(f"HTML content: {content}")
                 soup = BeautifulSoup(content, 'lxml')
 
-                important_tags = ['p', 'li', 'div', 'span']
+                important_tags = ['body', 'p', 'li', 'ul', 'div', 'span']
                 important_text = ''
 
                 for tag in important_tags:
