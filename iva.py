648f4afd8710ca128200940a8e4aaa8f17964c5c
bash <4gutzees@gmail.com>
2023-05-23 22:00:47 -0400

    enable webpage window

--- iva.py
+++ iva.py
@@ -804,14 +804,14 @@ async def iva(interaction: discord.Interaction, prompt: str, file_one: discord.A
             coroutine=parse_qa_webpage_input,
             description=QA_WEBPAGE_ASK_TOOL_DESCRIPTION,
         ))
-        """
+        
         tools.append(Tool(
             name = "Webpage Window",
             func=dummy_sync_function,
             coroutine=parse_view_webpage_input,
             description=WEBPAGE_WINDOW_ASK_TOOL_DESCRIPTION,
         ))
-        """
+        
         tools.append(Tool(
             name = "Python REPL",
             func=dummy_sync_function,
