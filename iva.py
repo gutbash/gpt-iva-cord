4368c5505de84b18070d1ef3fb251843954f0a91
bash <4gutzees@gmail.com>
2023-05-25 16:10:55 -0400

    account for newline chars in md image removal

--- iva.py
+++ iva.py
@@ -1070,7 +1070,7 @@ async def iva(interaction: discord.Interaction, prompt: str, file_one: discord.A
         reply = reply.replace("Do I need to use a tool? No", "")
         reply = reply.replace("```C#", "```cs")
         # Regex pattern for Markdown inline images
-        pattern = r'!\[.*?\]\(.*?\)'
+        pattern = r'\n?!\[.*?\]\(.*?\)'
         # Substituting the pattern with an empty string
         reply = re.sub(pattern, '', reply)
         
