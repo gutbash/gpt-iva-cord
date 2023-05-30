ebe91072db2cb7c46c272393bcbc517d3abd9122
bash <4gutzees@gmail.com>
2023-05-30 01:44:52 -0400

    remove beginning thought

--- constants.py
+++ constants.py
@@ -93,7 +93,7 @@ async def get_ask_suffix():
     
     Start responding below...
     ------
-    Thought: Does this require up to date information?{{agent_scratchpad}}
+    {{agent_scratchpad}}
     """
     return ask_suffix
     
