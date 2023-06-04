56b8536d5b4d14499d952ce77455da1fab659d6a
bash <4gutzees@gmail.com>
2023-06-04 09:58:34 -0400

    remove user input on tool response

--- constants.py
+++ constants.py
@@ -90,12 +90,7 @@ async def get_ask_suffix():
 async def get_template_tool_response():
     temple_tool_response = """TOOL RESPONSE: 
     ---------------------
-    {observation}
-    
-    USER'S INPUT
-    --------------------
-    
-    Okay, so what is the response to my last comment? If using information obtained from the tools you must mention it explicitly without mentioning the tool names - I have forgotten all TOOL RESPONSES!"""
+    {observation}"""
     return temple_tool_response
     
 ### CHAT PROMPT COMPONENTS ###
